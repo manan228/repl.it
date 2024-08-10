64384e63d538bcb4aad4988c326d1667984b7061
Manan Shah <manan228@yahoo.in>
2024-08-09 23:47:52 -0700

    updating orchestrator url

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,7 +1,7 @@
 export const SERVICE_URL = "https://repl-it.onrender.com";
 export const GET_AVAILABLE_REPLIT = `${SERVICE_URL}/get-available-replit`;
 
-export const ORCHESTRATOR_URL = `http://localhost:3002/start`
+export const ORCHESTRATOR_URL = `https://repl-it-orchestrator.onrender.com/start`
 
 export const BASE_DOMAIN = `shahmanan.tech`;
 
