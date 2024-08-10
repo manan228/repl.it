b0a724d53208f6ad1d1f15277250ac5f94a65918
Manan Shah <manan228@yahoo.in>
2024-08-09 22:30:56 -0700

    adding init-service hosted url

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,4 +1,4 @@
-export const SERVICE_URL = "http://localhost:3001";
+export const SERVICE_URL = "https://repl-it.onrender.com";
 export const GET_AVAILABLE_REPLIT = `${SERVICE_URL}/get-available-replit`;
 
 export const ORCHESTRATOR_URL = `http://localhost:3002/start`
